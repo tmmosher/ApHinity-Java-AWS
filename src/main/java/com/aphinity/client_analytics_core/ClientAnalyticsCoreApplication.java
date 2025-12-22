4601ae72940d987ca03f2b68ef93f54e5ac064c7
Trenton Mosher <starkbewegung@hotmail.com>
2025-12-22 13:28:16 -0700

    Added gradle and springboot setup with additions to gitignore.

---
+++ src/main/java/com/aphinity/client_analytics_core/ClientAnalyticsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.aphinity.client_analytics_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAnalyticsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAnalyticsCoreApplication.class, args);
+	}
+
+}
